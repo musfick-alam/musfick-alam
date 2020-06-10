60df797b6d5d6ddb0f5b9505ba53dcd53425d7f7
musfick-alam <66725983+musfick-alam@users.noreply.github.com>
2020-06-10 16:37:15 +0600

    Add files via upload

---
+++ assignment-1 2/js/java.js
@@ -0,0 +1,13 @@
+function a(msg) {
+	x=document.getElementById('change').className= msg;
+	// x.className= msg;
+}
+// }
+// function b() {
+// 	x=document.getElementById('change');
+// 	x.className="middle";
+// }
+// function c() {
+// 	x=document.getElementById('change');
+// 	x.className="bottom";
+// }
